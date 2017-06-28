4802c739f6fc3de102a8c5e15044d79b94feb474
Vadim Emelyanov <ve_git@mail.ru>
2017-06-28 11:19:56 +0400

    Изменены имена файлов

---
+++ src/Row.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Col from './Col';
+
+export default class Row extends Component {
+  static propTypes = {
+    data: PropTypes.arrayOf(PropTypes.string).isRequired
+  };
+
+  render() {
+    const { data } = this.props;
+    let cols = data.map(
+      (item, index) => <Col key={index} data={item} /> 
+    );
+    
+    return (
+      <tr>
+        {cols}
+      </tr>    
+    );  
+  };
+}
