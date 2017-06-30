2ce5cffa0044e57b5bc9831a5e354b1be2a308c1
Vadim Emelyanov <ve_git@mail.ru>
2017-06-30 17:52:41 +0400

    Работа с Redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import setup from './setup';
+import {fillTableFromArray} from './setup';
 import Table from './Table';
 
 class App extends Component {
@@ -13,26 +13,51 @@ class App extends Component {
  
   componentDidMount(){
     console.log('componentDidMount');
-/* Сначала вызову промис и обработаю его, в самом конце - вызов setState*/
-//http://export.rbc.ru/free/cb.0/free.fcgi?
-    axios({
-      method: 'GET',
-      url: 'https://jsonplaceholder.typicode.com/posts/1'
-    }).then((response) => {
-      console.log('i am here!');
-      /*
-      console.log('response');
-      for (let key in response) {
-        console.log('key='+key+' response[key]='+response[key]);
+    console.log('test start');
+
+
+    /*
+    TODO вынести в отдельный модуль заполнение массива
+    из внешнего источника данных и заполнение таблицы
+    из этого массива.
+    */
+
+
+
+    let newWords = [];
+    let words = [];
+    const colsCount = 4;
+    const rowsCount = 10;
+    const maxWordsCount = colsCount * rowsCount;
+
+    const loop = () =>
+      axios({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/posts/1'
+      })
+      .then(response => {
+        newWords = (response.data.body).split(/\s/);
+        words = words.concat(newWords);
+        
+        if ((newWords.length===0)||(words.length>=maxWordsCount)){
+          return words;
+        }else{
+          return loop();
+        }
+      });
+
+    loop().then(() => {
+      //words.map((value, index) =>  console.log('words['+index+']='+value));
+      let newData = {
+        caption: 'Заголовок',
+        colNames: ['I', 'II', 'III', 'IV'],
+        cells: []
       };
-      */
-      console.log('response[data]');
-      for (let key in response.data) {
-        console.log('key='+key+' response.data[key]='+response.data[key]);
-      };
+      fillTableFromArray(newData.cells, 10, 4, words);
+      this.setState({data: newData});
     }).catch(error => {
       console.log(error);
-    });    
+    });
   };
 
   render() {
@@ -42,23 +67,6 @@ class App extends Component {
     return (
       <Table data={data}/>
     );
-  }
-}
-
-/*
-    axios({
-      method: 'get',
-      url: 'https://api.vk.com/method/groups.get',
-      params: {
-        access_token: accessToken,
-        v: '5.64',
-        count: '1'
-      }
-    }).then((response) => {
-      if (!response.data.response.count) {
-        throw new Error('╨ö╨░╨╜╨╜╤ï╨╡ ╨╜╨╡ ╨┐╨╛╨╗╤â╤ç╨╡╨╜╤ï. ' + response.data.error + ' : ' + response.data.error_description);
-      }
-      groupId = response.data.response.items[0];
-      res
-*/
+  };
+};
 export default App;
--- src/setup.js
+++ src/setup.js
@@ -1,16 +1,30 @@
-const setup = function(){
-  return (
-    {
-      caption: 'Заголовок',
-      colNames: ['I', 'II', 'III', 'IV'],
-      cells: [
-        ['1', '2', '3'],
-        ['4', '5', '6', 'extra'],
-        ['7', '8', '9'],
-        ['10', '11', '12']
-      ]
-    }
-  )  
+/*
+const setup = function(callback){
+  return ({
+    caption: 'Заголовок',
+    colNames: ['I', 'II', 'III', 'IV'],
+    cells: [
+      ['1', '2', '3'],
+      ['4', '5', '6', 'extra'],
+      ['7', '8', '9'],
+      ['10', '11', '12']
+    ]
+  }
+  );
 };
+*/
 
-export default setup;
+exports.fillTableFromArray = function(table, rowsCount, colsCount, source){
+  let elemCount = source.length;
+  let i, j; 
+  let k = 0;
+  for (i=0; i<rowsCount; ++i){
+    table[i] = [];
+    for (j=0; j<colsCount; ++j){
+      if (k === elemCount){ break;}
+      table[i][j] = source[k];
+      ++k;
+    }
+    if (k === elemCount){ break;}
+  }
+};
