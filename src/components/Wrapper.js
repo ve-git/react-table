4a2eaf61482ff9a858cd329d9ee91167fab38a35
Vadim Emelyanov <ve_git@mail.ru>
2017-07-11 16:28:10 +0400

    Вместо стейта сейчас используется свойство объекта компонента

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -4,20 +4,17 @@ import Table from './Table';
 import {colsCount} from '../components/setup';
 
 export default class Wrapper extends Component {
-
   constructor(props){
     super(props);
-    this.state = {
-      colParams: {
-        elements: [],
-        colRight: [],
-        bottom: 0,
-        activeColNum: -1,  
-        changingState: -1, // 0 - none (colsInit finished), 1 - ready, 2 - is changing, 
-                           //-1 - initial
-        savedWidth: 0  // width of active column before changing
-      },
-      dummy: 0
+    this.params = {
+      elTable: null,  
+      elements: [],
+      colRight: [],
+      bottom: 0,
+      activeColNum: -1,  
+      changingState: -1, // 0 - none (colsInit finished), 1 - ready, 2 - is changing, 
+                         //-1 - initial
+      savedWidth: 0  // width of active column before changing
     };
     this.colsElementInit = this.colsElementInit.bind(this);
     this.colsMouseMove = this.colsMouseMove.bind(this);
@@ -25,6 +22,8 @@ export default class Wrapper extends Component {
     this.colsMouseUp = this.colsMouseUp.bind(this);
     this.colsMouseOut = this.colsMouseOut.bind(this);
     this.setAddress = this.setAddress.bind(this);
+    this.recalcColRight = this.recalcColRight.bind(this);
+    this.recalcState = this.recalcState.bind(this);
   }  
 
   static propTypes = {
@@ -35,47 +34,44 @@ export default class Wrapper extends Component {
     }).isRequired
   }
 
-  colsElementInit(element) {
-    if (this.state.colParams.changingState === 0) return;   
-    let colParams = {...this.state.colParams};
-    if (!element) { return; }
-    for(let i=0; i<element.children.length; i++) {
-      colParams.elements[i] = element.children[i];    
+  componentDidMount(){
+    this.colsElementInit();
+  }
+
+  recalcColRight(){
+    let params =  this.params;
+    let elTr = params.elTable.children[1].children[0]; // /table/thead/tr      
+    for(let i=0; i<elTr.children.length; i++) {
+      params.elements[i] = elTr.children[i];    
     }
-
-    const headCoords = {
-      left: element.offsetLeft,
-      top: element.offsetTop,
-      width: element.offsetWidth,
-      height: element.offsetHeight
-    };
-
-    // Определим величину colRight
-    colParams.colRight = [];
+    params.colRight = [];
     for (let i=1; i<colsCount; ++i){
-      colParams.colRight[i-1] = colParams.elements[i].offsetLeft;
+      params.colRight[i-1] = params.elements[i].offsetLeft;
     }
-    colParams.colRight[colsCount-1] = headCoords.left + headCoords.width;
+    params.colRight[colsCount-1] = elTr.offsetLeft + elTr.offsetWidth;
+  }
 
-    let elTable = colParams.elements[0].parentNode.parentNode.parentNode; 
-    colParams.bottom = elTable.offsetTop + elTable.offsetHeight;
-    colParams.changingState = 0;
-    this.setState({colParams});
+  colsElementInit() {
+    let params =  this.params;
+    params.elTable = this.address.children[0]; // /div/table
+    params.bottom = params.elTable.offsetTop + params.elTable.offsetHeight;
+    this.recalcColRight();
+    params.changingState = 0;
   };
 
   colsMouseMove(event) {
     event.preventDefault();
-    let colParams = {...this.state.colParams};
-    let {activeColNum, changingState} = colParams;
+    let params =  this.params;
+    let {activeColNum, changingState} = params;
     const x = event.pageX;
 
-    if ((colParams.changingState === 0) || (colParams.changingState === 1)){
+    if ((params.changingState === 0) || (params.changingState === 1)){
       let found = false;
       let newChangingState = 0;
       let newActiveColNum = -1;
       
       for (let i=0; i<colsCount; ++i){
-        if (Math.abs(x - colParams.colRight[i]) < 20) {
+        if (Math.abs(x - params.colRight[i]) < 20) {
           found = true;
           newActiveColNum = i;  
           newChangingState = 1;
@@ -83,89 +79,59 @@ export default class Wrapper extends Component {
         }
       }
       if (found){
-        colParams.elements[0].parentNode.parentNode.parentNode.style.cursor='w-resize';
+        params.elTable.style.cursor='w-resize';
       }
       else{
-        colParams.elements[0].parentNode.parentNode.parentNode.style.cursor='auto';
+        params.elTable.style.cursor='auto';
       }
-      if ((activeColNum !== newActiveColNum) || (changingState||newChangingState)){
-        colParams.activeColNum = newActiveColNum;
-        colParams.changingState = newChangingState;
-        this.setState({colParams});
-
+      if ((activeColNum !== newActiveColNum) || (changingState !== newChangingState)){
+        params.activeColNum = newActiveColNum;
+        params.changingState = newChangingState;
       }
-    } else if (colParams.changingState === 2) {
-      //TODO избежать ненужного копирования стейта  
-      colParams.elements[activeColNum].width = x - colParams.colRight[activeColNum] + colParams.savedWidth;
+    } else if (params.changingState === 2) {
+      params.elements[activeColNum].width = x - params.colRight[activeColNum] + params.savedWidth;
     }
   };
 
-
   colsMouseDown(event) {
     event.preventDefault();
-    let colParams = {...this.state.colParams};
-    if (colParams.changingState !== 1) return;
-    let { activeColNum } = colParams;
+    let params =  this.params;
+    if (params.changingState !== 1) return;
+    let { activeColNum } = params;
     if (activeColNum===0){
-      colParams.savedWidth=colParams.colRight[0];
+      params.savedWidth=params.colRight[0];
     } else {
-      colParams.savedWidth = colParams.colRight[activeColNum] - colParams.colRight[activeColNum-1];
+      params.savedWidth = params.colRight[activeColNum] - params.colRight[activeColNum-1];
     }
-    colParams.changingState = 2;
-    this.setState({colParams});
+    params.changingState = 2;
   }
 
-  recalcState(colParams){
-    colParams.elements[0].parentNode.parentNode.parentNode.style.cursor='auto';
-    const parentNode = colParams.elements[0].parentNode;
-    const headCoords = {
-      left: parentNode.offsetLeft,
-      top: parentNode.offsetTop,
-      width: parentNode.offsetWidth,
-      height: parentNode.offsetHeight
-    };
-    colParams.colRight = [];
-    for (let i=1; i<colsCount; ++i){
-      colParams.colRight[i-1] = colParams.elements[i].offsetLeft;
-    }
-    colParams.colRight[colsCount-1] = headCoords.left + headCoords.width;
-    colParams.changingState = 0;
-    this.setState({colParams});
+  recalcState(){
+    let params =  this.params;
+    params.elTable.style.cursor='auto';
+    this.recalcColRight();
+    params.changingState = 0;
   }
 
   colsMouseUp(event) {
     event.preventDefault();
-    let colParams = {...this.state.colParams};
-    if (colParams.changingState !== 2) return;
-    this.recalcState(colParams);
+    if (this.params.changingState !== 2) return;
+    this.recalcState();
   }    
 
   colsMouseOut(event) {
-    let colParams = {...this.state.colParams};
     const y = event.pageY;
-    if (y < colParams.bottom) return;  //Некорректный вызов события
-    if (colParams.changingState !== 2) return;
-    this.recalcState(colParams);
+    if (y < this.params.bottom) return;  //Некорректный вызов события
+    if (this.params.changingState !== 2) return;
+    this.recalcState();
   }    
 
   setAddress(elem){
     this.address = elem;
   }
-
-  componentDidMount(){
-    console.log('componentDidMount');
-    //                 /div    /table     /thead       /tr
-    let element = this.address.children[0].children[1].children[0];
-    this.colsElementInit(element);
-  }
-
-  shouldComponentUpdate(nextProps, nextState){
-    return false;
-  }
-
+ 
   render() {
     console.log('render Wrapper');
-      
     const data = this.props.data;
     return (
       <div 
@@ -183,4 +149,3 @@ export default class Wrapper extends Component {
     );
   };
 };
-
